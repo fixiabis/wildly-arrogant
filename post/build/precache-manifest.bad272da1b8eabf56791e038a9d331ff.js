dbf67dc03e8c1eae64e1287db66d51145fb78920
fixiabis <fixiabis@gmail.com>
2020-06-13 11:20:25 +0800

    new build

---
+++ post/build/precache-manifest.bad272da1b8eabf56791e038a9d331ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969cf544ff59544273858b68ecfce0c9",
+    "url": "/wildly-arrogant/post/index.html"
+  },
+  {
+    "revision": "be56ea0d0324b3fdb718",
+    "url": "/wildly-arrogant/post/static/css/main.30713401.chunk.css"
+  },
+  {
+    "revision": "1a1a85bdcde7eeecdff0",
+    "url": "/wildly-arrogant/post/static/js/2.3eb1c9ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wildly-arrogant/post/static/js/2.3eb1c9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be56ea0d0324b3fdb718",
+    "url": "/wildly-arrogant/post/static/js/main.4ff09254.chunk.js"
+  },
+  {
+    "revision": "353cc2229fe946f7b71b",
+    "url": "/wildly-arrogant/post/static/js/runtime-main.27796187.js"
+  }
+]);
